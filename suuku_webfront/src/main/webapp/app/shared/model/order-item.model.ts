1fb2246d92e33136496bc47d7b7c7b0cdcbdc939
Francis Anyuru <anyuruf@gmail.com>
2025-08-22 06:51:51 -0400

    Updated the html title to suuku_commerce and corrected paths imports in the models package

--- suuku_webfront/src/main/webapp/app/shared/model/order-item.model.ts
+++ suuku_webfront/src/main/webapp/app/shared/model/order-item.model.ts
@@ -1,5 +1,6 @@
-import { IProduct } from 'app/shared/model/suuku_product/product.model';
-import { IProductOrder } from 'app/shared/model/suuku_product/product-order.model';
+// Update the import path below to the correct location of product.model.ts
+import { IProduct } from 'app/shared/model/product.model';
+import { IProductOrder } from 'app/shared/model/product-order.model';
 import { OrderItemStatus } from 'app/shared/model/enumerations/order-item-status.model';
 
 export interface IOrderItem {
