dc7b5c9e1a0285332547ab6a738a9a51095250e0
Francis Anyuru <anyuruf@gmail.com>
2025-08-22 07:09:09 -0400

    Add types to conf/error-middleware function

--- suuku_webfront/src/main/webapp/app/config/error-middleware.ts
+++ suuku_webfront/src/main/webapp/app/config/error-middleware.ts
@@ -1,14 +1,52 @@
-const getErrorMessage = errorData => {
+interface GetErrorMessageFieldError {
+  field: string;
+  objectName: string;
+  message: string;
+}
+
+interface GetErrorMessageErrorData {
+  message: string;
+  fieldErrors?: GetErrorMessageFieldError[];
+}
+
+const getErrorMessage = (errorData: GetErrorMessageErrorData): string => {
   let { message } = errorData;
   if (errorData.fieldErrors) {
-    errorData.fieldErrors.forEach(fErr => {
+    errorData.fieldErrors.forEach((fErr: GetErrorMessageFieldError) => {
       message += `\nfield: ${fErr.field},  Object: ${fErr.objectName}, message: ${fErr.message}\n`;
     });
   }
   return message;
 };
 
-export default () => next => action => {
+interface FieldError {
+  field: string;
+  objectName: string;
+  message: string;
+}
+
+interface ErrorData {
+  message: string;
+  fieldErrors?: FieldError[];
+}
+
+interface ErrorResponse {
+  data: ErrorData;
+}
+
+interface Action {
+  type: string;
+  error?: {
+    message?: string;
+    response?: ErrorResponse;
+  };
+  [key: string]: any;
+}
+
+type MiddlewareAPI = (action: Action) => any;
+type Next = (action: Action) => any;
+
+export default (): ((next: Next) => (action: Action) => any) => next => action => {
   /**
    *
    * The error middleware serves to log error messages from dispatch
